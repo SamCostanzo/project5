c5de464972429832c0ca790d8d0a5e2af2be3635
Sam Costanzo <samcostanzo25@gmail.com>
2019-02-25 17:18:58 -0500

    5:18pm

--- js/app.js
+++ js/app.js
@@ -1,50 +1,34 @@
 /*********************************************AWESOME STARTUP EMPLOYEE DIRECTORY******************************************/
 
+const gallery = document.getElementById('gallery');
+const card = gallery.getElementsByClassName('card');
 
-// // Connecting and getting a random user from the given API
-// fetch('https://randomuser.me/api/')  //Fetches the API's url
-//     .then(response => response.json())   //Turns that response into json
-//     .then(data => console.log(data.results));   //Logs the "results" of that data. This is the data we can work with. A random user.
-
-
-// Just for testing. Logging the array of people from the API
-fetch('https://randomuser.me/api/?results=12')
-    .then(response => response.json())
-    .then(body => console.log(body))
 
 // Using the fetch method to call the random user API and get 12 results or users back. Using forEach() to itterate over each person and put their info into the generateUserCard() function
 fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
-    // .then(body => console.log(body))
     .then(body => body.results)
     .then(results => {
+        console.log(results);
+        let users = results;
         results.forEach(result => {
-            generateUserCard(result.name.first + ' ' + result.name.last, result.picture.medium, result.email, result.location.city, + ', ' + result.location.state);
-            // I think we would add the click event listener here for the modal window?
-            
-        });
-    })
-    
-
-    // WIP event listener 
-    const gallery = document.getElementById('gallery');
-    const card = gallery.getElementsByClassName('card');
-    for(let i = 0; i < card.length; i++){
+            generateUserCard(result.name.first + ' ' + result.name.last, result.picture.medium, result.email, result.location.city, result.location.state);
+        })
+        
+        // Click event listner for modal window function call
+        const gallery = document.getElementById('gallery');
+        const card = gallery.getElementsByClassName('card');
+        for (let i = 0; i < card.length; i++) {
             card[i].addEventListener('click', () => {
-            generateModalWindow(result.name.first + ' ' + result.name.last, result.email, result.location.city, result.cell, result.location.street + ', ' + result.location.state + result.location.postcode, result.dob.date);  // Pass in info for modal window
-            console.log('Test');
-        });
-    }
-
-    
-    
-    
-    // card.addEventListener('click', () => {
-    //     // generateModalWindow(result.name.first + ' ' + result.name.last, result.email, result.location.city, result.cell, result.location.street + ', ' + result.location.state + result.location.postcode, result.dob.date);  // Pass in info for modal window
-    //     alert('You got it');
-    // });
-
-
+                // formating the birthday to display correctly on the modal window
+                let dob_year = results[i].dob.date.slice(2, 4);
+                let dob_month = results[i].dob.date.slice(5, 7);
+                let dob_day = results[i].dob.date.slice(8, 10);                
+                // Calling and passing in all the users info for the modal window
+                generateModalWindow(results[i].name.first + ' ' + results[i].name.last, results[i].picture.medium, results[i].email, results[i].location.city, results[i].cell, results[i].location.street + ', ' + results[i].location.state + ' ' + results[i].location.postcode, 'Birthday: ' + dob_month + '/' + dob_day + '/' + dob_year); 
+            });
+        }
+    })
 
 
 /**********************************************************************FUNCTIONS********************************************************************/
@@ -56,17 +40,17 @@ function setAttributes(el, attrs) {
     }
   }
 
-  // Creating the search bar and appending it to the HTML
+// Creating the search bar and appending it to the HTML
 function createSearchBar(){
     // Creating form to hold search and submit inputs
     const form = document.createElement('form');
     setAttributes(form, {'action':'#', 'method':'get'});
     // Creating the forms input 'search' field
     const formInput = document.createElement('input');
-    setAttributes(formInput, {'type':'search', 'id':'search-input', 'placeholder':'Search...'});
+    setAttributes(formInput, {'type':'search', 'id':'search-input', 'placeholder':'Find an employee!', "onFocus":"placeholder = ''"});
     // Creating the forms 'submit' input field
     const formSubmit = document.createElement('input');
-    setAttributes(formSubmit, {'type':'submit', 'value':'&#x1F50D;', 'id':'Search-submit', 'class':'search-submit'});
+    setAttributes(formSubmit, {'type':'submit', 'value':'Search', 'id':'Search-submit', 'class':'search-submit'});
     // Appending the two inputs to the form, and then to the HTML div
     form.appendChild(formInput);
     form.appendChild(formSubmit);
@@ -113,66 +97,85 @@ function generateUserCard(name, picture, email, city, state){
 }
 
 // Modal window function
-function generateModalWindow(name, email, city, phoneNumber, adress, dob){
+function generateModalWindow(name, picture, email, city, phoneNumber, adress, dob){
     // Creating main container div
     const modalContainer = document.createElement('div');
     setAttributes(modalContainer, {'class':'modal-container'});
-    
     // Creating modal div
     const modalDiv = document.createElement('div');
     setAttributes(modalDiv, {'class':'modal'});
-
     // Creating the X or exit button for the window
     const Xbutton = document.createElement('button');
     setAttributes(Xbutton, {'type':'button', 'id':'modal-close-btn', 'class':'modal-close-btn'});
     // Add strong on the button?
     Xbutton.textContent = 'X';
-
     // Creating div for modal-info-container
     const modalInfoContainer = document.createElement('div');
     setAttributes(modalInfoContainer, {'class':'modal-info-container'});
-
     // Creating the img or profile picture
     const img = document.createElement('img');
-    setAttributes(img, {'class':'modal-img', 'src':'https://placehold.it/125x125', 'alt':'profile picture'});   // Pass in profile picture to src
-
+    setAttributes(img, {'class':'modal-img', 'src': picture, 'alt':'profile picture'});   // Pass in profile picture to src
     // Create h3 for the name
     const h3 = document.createElement('h3');
-    setAttributes(h3, {'id':'name', '':'modal-name cap'});
-    h3.textContent = '';   // Pass in name
-
+    setAttributes(h3, {'id':'name', 'class':'modal-name cap'});
+    h3.textContent = name;   // Pass in name
     // Create p for email
     const p_email = document.createElement('p');
     setAttributes(p_email, {'class':'modal-text'});
-    p_email.textContent = '';   // Pass in email       
-    
+    p_email.textContent = email;   // Pass in email       
     // Create p for city
     const p_city = document.createElement('p');
     setAttributes(p_city, {'class':'modal-text cap'});
-    p_city.textContent = '';   // Pass in city     
-
-    // Create <hr>? Not sure if this will work
-
+    p_city.textContent = city;   // Pass in city     
+    // Create <hr>
     const hr = document.createElement('hr');
-
-
     // Create p for phone number
     const p_phoneNumber = document.createElement('p');
     setAttributes(p_phoneNumber, {'class':'modal-text'});
-    p_phoneNumber.textContent = '';   // Pass in phone number   
-
-
+    p_phoneNumber.textContent = phoneNumber;   // Pass in phone number   
     // Create p for fill adress
     const p_adress = document.createElement('p');
     setAttributes(p_adress, {'class':'modal-text'});
-    p_adress.textContent = '';   // Pass in adress   
-
-
+    p_adress.textContent = adress;   // Pass in adress   
     // Create p for DoB
     const p_dob = document.createElement('p');
     setAttributes(p_dob, {'class':'modal-text'});
-    p_dob.textContent = '';   // Pass in dob   
+    p_dob.textContent = dob;   // Pass in dob   
+    
+    // For exceeds grade. Adding the next and prev buttons for the window
+    // Create the div 
+    const modalButtonContainer = document.createElement('div');
+    setAttributes(modalButtonContainer, {'class':'modal-btn-container'});
+    // Create the two buttons
+    // Previous
+    const previous = document.createElement('button');
+    setAttributes(previous, {'type':'button', 'id':'modal-prev', 'class':'modal-prev btn'});
+    previous.textContent = 'Prev';
+    // Next
+    const next = document.createElement('button');
+    setAttributes(next, {'type':'button', 'id':'modal-next', 'class':'modal-next btn'});
+    next.textContent = 'Next';
+    // Append to modal window
+    $(modalButtonContainer).append(previous);
+    $(modalButtonContainer).append(next);
+    $(modalContainer).append(modalButtonContainer);
 
+    // Click event listeners on the prev and next buttons
+    previous.addEventListener('click', () => {
+        console.log('prev');
+        // let prevResult = users - 1;  
+        // generateModalWindow(prevResult.name.first + ' ' + this.result.name.last, this.result.picture.medium, this.result.email, this.result.location.city, this.result.cell, this.result.location.street + ', ' + this.result.location.state + ' ' + this.result.location.postcode, 'Birthday: ' + dob_month + '/' + dob_day + '/' + dob_year); 
+    });
+
+    next.addEventListener('click', () => {
+        console.log('next');
+        // let prevResult = users - 1;  
+        // generateModalWindow(prevResult.name.first + ' ' + this.result.name.last, this.result.picture.medium, this.result.email, this.result.location.city, this.result.cell, this.result.location.street + ', ' + this.result.location.state + ' ' + this.result.location.postcode, 'Birthday: ' + dob_month + '/' + dob_day + '/' + dob_year); 
+    });
+
+
+    
+    
     // Append all the display elements to the first div modalInfoContainer
     $(modalInfoContainer).append(img);
     $(modalInfoContainer).append(h3);
@@ -187,6 +190,12 @@ function generateModalWindow(name, email, city, phoneNumber, adress, dob){
     $(modalDiv).append(Xbutton);
     // Finally, append all of that to the main container div for the modal window. Keep in mind the next and prev buttons need to be appended to this as well
     $(modalContainer).append(modalDiv);
+    $(gallery).append(modalContainer);
+
+    // Click event listener for the Xbutton to close the modal window
+    Xbutton.addEventListener('click', () => {
+        $('.modal-container').remove();
+    });
 }
 
 
